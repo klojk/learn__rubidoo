a1a025018f179583b8b59784b13823f566233edc
klojk <35073921+klojk@users.noreply.github.com>
2018-01-16 19:47:10 +0100

    Update simon_says.rb

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,31 @@
-#write your code here
+def add(x,y)
+	x+y
+end
+
+def subtract(x,y)
+	x-y
+end
+
+
+def sum(array)
+  result = 0
+  i = 0
+  while i < array.length
+    result += array[i]
+    i += 1
+ end
+ return result
+end
+
+def titleize(str)
+	res = []
+	words = str.downcase.split(" ")
+	words.each_with_index do |x, i|
+		if x.length > 4 || i == 0 || i == words.length - 1
+			res << x.capitalize
+		else
+			res << x
+		end
+	end
+	res.join(' ')
+end

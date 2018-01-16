f35b86842090f5175c72b6d63ecfdf0e588a9266
klojk <35073921+klojk@users.noreply.github.com>
2018-01-16 19:45:36 +0100

    Update calculator.rb

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,19 @@
-#write your code here
+def add(x,y)
+	x+y
+end
+
+def subtract(x,y)
+	x-y
+end
+
+
+def sum(array)
+  result = 0
+  i = 0
+  while i < array.length
+    result += array[i]
+    i += 1
+ end
+ return result
+end
+
